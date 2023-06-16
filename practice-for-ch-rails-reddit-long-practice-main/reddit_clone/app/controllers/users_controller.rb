a1915c6360060c2bab9b923533f7c03c5f42ceda
Jordy <jordycorporan95@gmail.com>
2023-06-16 11:16:46 -0400

    Added UsersController

---
+++ practice-for-ch-rails-reddit-long-practice-main/reddit_clone/app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+  before_action :require_logged_in, :only[:destroy,:show,:index,:update,:edit]
+  before_action :require_logged_out, :only[:create,:new]
+
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user
+      redirect_to user_url
+    else
+      render :new
+    end
+  end
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  def user_params
+    params.require(:user).permit(:username,:password)
+  end
+
+
+
+
+end
